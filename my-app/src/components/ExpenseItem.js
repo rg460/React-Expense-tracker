89ef820047cd4a1bf7e32289500a7cfc6fe5049c
ravigordon <rgordz460@outlook.com>
2022-06-20 10:46:08 +0100

    dynamic data addded to ExpenseItem.js

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -3,13 +3,15 @@ import './ExpenseItem.css';
 
 const ExpenseItem = () => {
     const expenseDate = new Date(2022,6,20);
+    const expenseTitle = 'Monthly Udemy Learning'
+    const expenseAmount = 26.99;
   return (
     <div className="expense-item">
-      <div>June 20th 2022</div>
+      <div>{expenseDate }</div>
       <div className="expense-item__description">
-        <h2>Monthly Udemy Learning</h2>
+        <h2>{expenseTitle}</h2>
       </div>
-      <div className="expense-item__price">£26.99</div>
+      <div className="expense-item__price">{expenseAmount}</div>
     </div>
   );
 };
