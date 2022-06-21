9a9863c2d20cbce388ba99d52a636b58c9ecfffc
ravigordon <rgordz460@outlook.com>
2022-06-21 11:06:09 +0100

    title and amount refactored as props

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,17 +1,15 @@
 import React from "react";
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
-    const expenseDate = new Date(2022,6,20);
-    const expenseTitle = 'Monthly Udemy Learning'
-    const expenseAmount = 26.99;
+const ExpenseItem = (props) => {
+    
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      {/* <div>{props.date.toISOString()}</div> */}
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
       </div>
-      <div className="expense-item__price">{expenseAmount}</div>
+      <div className="expense-item__price">{props.amount}</div>
     </div>
   );
 };
