0b11d18a30bb2fe0b935a7dc60d7e4a5c4669358
ravigordon <rgordz460@outlook.com>
2022-06-21 11:18:02 +0100

    date bug now fixed so date now renders on the page

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -5,7 +5,7 @@ const ExpenseItem = (props) => {
     
   return (
     <div className="expense-item">
-      {/* <div>{props.date.toISOString()}</div> */}
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
