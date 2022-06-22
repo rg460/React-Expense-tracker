ec8f13c4b8fc36cc3e5676692610309e48be9b54
ravigordon <rgordz460@outlook.com>
2022-06-22 09:50:12 +0100

    Bug Fixed in ExpenseItem.js page now renders

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
-import React from "react";
-import Expensedate from './ExpenseDate'
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-const ExpenseItem = (props) => {
+function ExpenseItem(props) {
   return (
-    <div className="expense-item">
-      <div className="expense-item__description">
+    <div className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
         <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <div className="expense-item__price">{props.amount}</div>
     </div>
   );
-};
+}
 
 export default ExpenseItem;
